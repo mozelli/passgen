c894664e8578ba27385f90d4c232cbd0ded937ad
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-03-01 19:07:22 -0300

    Update Footer credits

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,7 +4,12 @@ export default function Footer() {
     return (
         <footer className={ styles.container }>
             <div className={ styles.credits }>
-                Desenvolvido por <span>João Mozelli Neto</span>
+                <div>
+                    Desenvolvido por <br /><span>João Mozelli Neto</span>
+                </div>
+                <div className={ styles.copy }>
+                    Todos os direitos reservados - 2022&copy;
+                </div>
             </div>
         </footer>
     )
