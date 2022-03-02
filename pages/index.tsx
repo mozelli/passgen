48d50047fdc28abe4b64d261763d3cf9a81b3b66
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-03-02 16:24:15 -0300

    Layout modifications in Home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import SEO from '../components/SEO';
 import styles from './home.module.scss'
 
 export default function Home() {
-  const [size, setSize] = useState<any>(6);
+  const [size, setSize] = useState<any>(4);
   const [password, setPassword] = useState("");
 
   useEffect(() => {
@@ -27,7 +27,7 @@ export default function Home() {
   }
 
   function minus() {
-    if (size >= 2) {
+    if (size >= 5) {
       const sizeMinusOne = size - 1;
       setSize(sizeMinusOne);
     }
@@ -42,23 +42,28 @@ export default function Home() {
   return (
     <div className={ styles.container }>
       <SEO title='PassGen' description='Gerador de password online. Gerador de senha online.' image='key-image.png' />
-      <div className={ styles.content }>
-        <div className={ styles.label }>
-          Tamanho:
+      <div>
+        <div className={ styles.smallText }>
+          Informe abaixo o número de caracteres da senha
         </div>
-        <div className={ styles.sizer }>
-          <button type="button" onClick={ minus } className="px-2 align-middle">
-            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="#295264" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z"/></svg>
-          </button>
-          <input 
-            type="number" 
-            value={ size } 
-            className="mt-1 py-2 px-3 w-16 text-center bg-white border border-slate-300 rounded-md shadow-sm" 
-            onChange={(e) => setSize(e.target.value)}
-          />
-          <button type="button" onClick={ plus } >
-            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="#295264" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/></svg>
-          </button>
+        <div className={ styles.content }>
+          <div className={ styles.label }>
+            Tamanho:
+          </div>
+          <div className={ styles.sizer }>
+            <button type="button" onClick={ minus } className="px-2 align-middle">
+              <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="#295264" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z"/></svg>
+            </button>
+            <input 
+              type="number" 
+              value={ size } 
+              className="mt-1 py-2 px-3 w-16 text-center bg-white border border-slate-300 rounded-md shadow-sm" 
+              onChange={(e) => setSize(e.target.value)}
+            />
+            <button type="button" onClick={ plus } >
+              <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="#295264" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/></svg>
+            </button>
+          </div>
         </div>
       </div>
       <div>
