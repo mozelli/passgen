96e0be64ddb23d967444d9b1cef61eabf30a7ea9
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-03-02 13:58:44 -0300

    Remove tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react'
 import SEO from '../components/SEO';
-import styles from '../styles/Home.module.scss'
+
 
 export default function Home() {
-  const [size, setSize] = useState<number>(6);
-  const [password, setPassword] = useState<string>("");
+  const [size, setSize] = useState<any>(6);
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
     setSize(size);
@@ -33,23 +33,30 @@ export default function Home() {
   }
 
   return (
-    <div className={styles.container}>
+    <div className="bg-violet-200">
       <SEO title='PassGen' description='Gerador de password online. Gerador de senha online.' image='key-image.png' />
-      <div className={ styles.field }>
-        <div className={ styles.label }>Tamanho:</div>
-        <button type="button" onClick={ plus }>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/></svg>
-        </button>
-        <input type="number" value={ size } disabled />
-        <button type="button" onClick={ minus }>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z"/></svg>
-        </button>
+      <div>
+        <div>Tamanho:</div>
+        <div className='align-center'>
+          <button type="button" onClick={ plus } className="px-2 align-middle">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/></svg>
+          </button>
+          <input 
+            type="number" 
+            value={ size } 
+            className="mt-1 py-2 px-3 w-16 text-center bg-white border border-slate-300 rounded-md shadow-sm" 
+            onChange={(e) => setSize(e.target.value)}
+          />
+          <button type="button" onClick={ minus } className="px-2 align-middle">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z"/></svg>
+          </button>
+        </div>
       </div>
-      <div className={ styles.result }>
-        <div className={ styles.password }>
+      <div>
+        <div>
           { password }
         </div>
-        { process.env.NEXT_PUBLIC_SITE_URL }
+
       </div>
       
     </div>
